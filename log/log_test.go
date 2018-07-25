a5fd8c3ba9eb7a989005d13910edf71346b32aff
maxjkfc <maxjkfc@gmail.com>
2018-07-25 11:20:24 +0800

    [refactor] refactor the log interface 1. remove the Zlog() and Zlogs() function 2. add the zlog.go file for zap log implement 3. add the config.go for common config setting

--- log/log_test.go
+++ log/log_test.go
@@ -2,8 +2,6 @@ package log
 
 import (
 	"testing"
-
-	"go.uber.org/zap"
 )
 
 type output struct {
@@ -11,24 +9,37 @@ type output struct {
 	Msg  string `json:"msg"`
 }
 
-func Test_NewZapConfig(t *testing.T) {
-	if err := NewZapConfig().Env(DEV).Level(DEBUG).Build(); err != nil {
-		t.Error(err)
-	}
-	t.Log("Success")
+func Test_initLog(t *testing.T) {
+	Log().Info("inti", nil)
+
 }
+
+func Test_NewZapConfig(t *testing.T) {
+	if _, err := NewZapConfig().Env(DEV).Level(DEBUG).Build(); err != nil {
+		t.Error(err)
+	} else {
+
+		t.Log("Success")
+
+	}
+}
+
 func Test_Zlog_Output(t *testing.T) {
-	Zlog().Info("Bug", zap.Any("context", output{
+
+	Log().Info("Bug", output{
 		Code: "1",
 		Msg:  "kkuu",
-	}))
+	})
 
 }
 
 func Test_Zlog(t *testing.T) {
-	Zlog().Info("Test", zap.Any("context", 123))
-}
-
-func Test_Zlogs(t *testing.T) {
-	Zlogs().Error("Test", "test2")
+	x := struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}{
+		"jimmy",
+		10,
+	}
+	Log().Info("Test", x)
 }
