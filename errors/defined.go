e7afe5aa4e120fe53dabf4c826eaec1b8d96acef
maxjkfc <maxjkfc@gmail.com>
2018-07-31 14:54:22 +0800

    [Feature] errors/defined.go add the NotFound Error

--- errors/defined.go
+++ errors/defined.go
@@ -5,6 +5,7 @@ package errors
 var (
 	NotError = New(0, "Success")
 
+	// Database Connect
 	ErrorConfigSet = New(102, "Config Seting Failed")
 
 	ErrorDataBaseType = New(200, "Error Database Connect Type")
@@ -12,8 +13,10 @@ var (
 	ErrorConnectFailed = New(201, "Connect Failed.")
 
 	// Database
-
-	ErrorPoolNotFound = New(210, "Not Find the DB in the Pool With the Tag Name")
+	ErrorPoolNotFound = New(210, "Not Found the DB in the Pool With the Tag Name")
 
 	ErrorCreateFailed = New(211, "Create Failed")
+
+	//
+	ErrorNotFound = New(220, "Not found this ID")
 )
