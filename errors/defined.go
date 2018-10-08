52ae0213fb83f64fccc93b4aae2236092fcafcb3
maxjkfc <maxjkfc@gmail.com>
2018-10-08 16:26:02 +0800

    [modifity] change ErrorDataIdFailed to ErrorDataIDFailed

--- errors/defined.go
+++ errors/defined.go
@@ -3,10 +3,8 @@ package errors
 // Default Error Code
 
 var (
-	NotError       = New(0, "Success")
-	ErrorConfigSet = New(2, "Config seting failed")
-
-	// JWT
+	NotError                    = New(0, "Success")
+	ErrorConfigSet              = New(2, "Config seting failed")
 	ErrorJwtCreateFailedForType = New(102, "Create jwt token failed (WrongType)")
 	ErrorJwtCreateTokenFailed   = New(103, "Create the token failed")
 	ErrorJwtStoreTokenFailed    = New(104, "Store the user token failed")
@@ -14,23 +12,16 @@ var (
 	ErrorJwtWrongIssuer         = New(107, "Wrong jwt issuer")
 	ErrorJwtValidFailed         = New(108, "Jwt validate failed")
 	ErrorJwtTokenNotFound       = New(109, "Jwt token not found")
-
-	// Database Connect
-	ErrorDataBaseType  = New(200, "Error database connect type")
-	ErrorConnectFailed = New(201, "Connect failed.")
-	// Database
-	ErrorPoolNotFound = New(210, "Not found the db in the pool with the tag name")
-	ErrorCreateFailed = New(211, "Create failed")
-	ErrorUpdateFailed = New(212, "Update data failed")
-	ErrorDeleteFailed = New(214, "Delete data failed")
-
-	ErrorDataNotFound = New(220, "Not found the data")
-	ErrorDataIdFailed = New(221, "Wrong data ID type")
-
-	// Request
-	ErrorRequestBindFailed  = New(300, "Request bind failed")
-	ErrorRequestValidFailed = New(301, "Request valid failed")
-
-	ErrorOperationFailed  = New(310, "Operation failed")
-	ErrorIllegalOperation = New(311, "Illegal operation")
+	ErrorDataBaseType           = New(200, "Error database connect type")
+	ErrorConnectFailed          = New(201, "Connect failed.")
+	ErrorPoolNotFound           = New(210, "Not found the db in the pool with the tag name")
+	ErrorCreateFailed           = New(211, "Create failed")
+	ErrorUpdateFailed           = New(212, "Update data failed")
+	ErrorDeleteFailed           = New(214, "Delete data failed")
+	ErrorDataNotFound           = New(220, "Not found the data")
+	ErrorDataIDFailed           = New(221, "Wrong data ID type")
+	ErrorRequestBindFailed      = New(300, "Request bind failed")
+	ErrorRequestValidFailed     = New(301, "Request valid failed")
+	ErrorOperationFailed        = New(310, "Operation failed")
+	ErrorIllegalOperation       = New(311, "Illegal operation")
 )
